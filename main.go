114e06645f0b4bcb7b7182085900636be63abc52
DoG-peer <dogeometry50@gmail.com>
2016-01-31 05:31:32 +0900

    refactoring

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"github.com/dullgiulio/pingo"
 	"log"
-	"path/filepath"
 	"time"
 )
 
@@ -18,49 +16,40 @@ func main() {
 		log.Fatal(perr)
 		return
 	}
-	plugins := []*pingo.Plugin{}
-	//tasks := []*AppTask{}
+
 	for _, pluginPath := range pluginPaths {
-		log.Println(pluginPath)
-
-		plug := pingo.NewPlugin("unix", pluginPath)
-		plugins = append(plugins, plug)
 		go func() {
-			task := Task{}
-			task.path = pluginPath
-			task.name = filepath.Base(pluginPath)
-			task.configFile = filepath.Join(app.PluginConfigDir, task.name)
-			task.dataFile = filepath.Join(app.DataDir, task.name)
-
-			plug.Start()
-			defer plug.Stop()
-			if err := plug.Call("Task.Configure", task.configFile, &task.err); err != nil {
+			task := LoadTask(pluginPath, app.PluginConfigDir, app.DataDir)
+			task.Start()
+			defer task.Stop()
+			if err := task.Configure(); err != nil {
 				log.Fatalf("Failed to configurePlugin %s", task.configFile)
 				return
 			}
+
 			for {
 				// main task
-				if err := plug.Call("Task.Main", "", &task.err); err != nil {
+				if err := task.Main(); err != nil {
 					log.Fatalln(err)
 					break
 				}
+
 				// log
-				if err := plug.Call("Task.SaveData", task.dataFile, &task.err); err != nil {
+				if err := task.SaveData(); err != nil {
 					log.Fatalln(err)
 					break
 				}
+
 				// change config
-				if err := plug.Call("Task.SaveConfig", task.configFile, &task.err); err != nil {
+				if err := task.SaveConfig(); err != nil {
 					log.Fatalln(err)
 					break
 				}
 
 				// wait
-				if err := plug.Call("Task.Interval", "", &task.interval); err != nil {
+				if err := task.Wait(); err != nil {
 					log.Fatalln(err)
 					break
-				} else {
-					time.Sleep(task.interval)
 				}
 			}
 		}()
--- plugin.go
+++ plugin.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/dullgiulio/pingo"
+	"path/filepath"
 	"time"
 )
 
@@ -12,4 +14,60 @@ type Task struct {
 	name       string
 	path       string
 	dataFile   string
+	plugin     *pingo.Plugin
+}
+
+// LoadTask initialize Task
+func LoadTask(pluginPath, pluginConfigDir, dataDir string) Task {
+	task := Task{}
+	task.path = pluginPath
+	task.name = filepath.Base(pluginPath)
+	task.configFile = filepath.Join(pluginConfigDir, task.name)
+	task.dataFile = filepath.Join(dataDir, task.name)
+	return task
+}
+
+// Start starts Task
+func (t *Task) Start() {
+	t.plugin = pingo.NewPlugin("unix", t.path)
+	t.plugin.Start()
+}
+
+// Stop stops Task
+func (t *Task) Stop() {
+	t.plugin.Stop()
+}
+
+// Main calls main task
+func (t *Task) Main() error {
+	return t.plugin.Call("Task.Main", "", &t.err)
+}
+
+// SaveData saves data
+func (t *Task) SaveData() error {
+	return t.plugin.Call("Task.SaveData", t.dataFile, &t.err)
+}
+
+// SaveConfig saves config
+func (t *Task) SaveConfig() error {
+	return t.plugin.Call("Task.SaveConfig", t.configFile, &t.err)
+}
+
+// ReadInterval checks wait interval
+func (t *Task) ReadInterval() error {
+	return t.plugin.Call("Task.Interval", "", &t.interval)
+}
+
+// Configure load config
+func (t *Task) Configure() error {
+	return t.plugin.Call("Task.Configure", t.configFile, &t.err)
+}
+
+// Wait waits until next step
+func (t *Task) Wait() error {
+	if err := t.ReadInterval(); err != nil {
+		return err
+	}
+	time.Sleep(t.interval)
+	return nil
 }
